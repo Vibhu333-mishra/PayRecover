bea6b6bf2d21c4bf0784e19a484470be99a50bae
VIBHU <vibhukantm@gmail.com>
2026-09-10 19:26:53 +0530

    Update PayrecoverAiApplication.java

--- src/main/java/com/payrecover/payrecoverai/PayrecoverAiApplication.java
+++ src/main/java/com/payrecover/payrecoverai/PayrecoverAiApplication.java
@@ -3,13 +3,6 @@ package com.payrecover.payrecoverai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Entry point of the whole backend.
- * @SpringBootApplication is a shortcut annotation that turns on:
- *  - component scanning (finds our @Service, @RestController, @Repository classes)
- *  - auto-configuration (Spring guesses sensible defaults from our dependencies)
- *  - the ability to run as a standalone app with an embedded Tomcat server
- */
 @SpringBootApplication
 public class PayrecoverAiApplication {
 
