dda2e4d4e4c83fa2308164d80a7e4280d8791385
VIBHU <vibhukantm@gmail.com>
2026-09-13 21:45:25 +0530

    Update ResourceNotFoundException.java

--- src/main/java/com/payrecover/payrecoverai/exception/ResourceNotFoundException.java
+++ src/main/java/com/payrecover/payrecoverai/exception/ResourceNotFoundException.java
@@ -1,11 +1,6 @@
 package com.payrecover.payrecoverai.exception;
 
-/**
- * Thrown when a payment (or any resource) is looked up by ID and doesn't exist.
- * RuntimeException means we don't have to declare "throws" everywhere --
- * Spring's GlobalExceptionHandler below catches it and turns it into a
- * proper 404 JSON response instead of a raw Java stack trace.
- */
+
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
